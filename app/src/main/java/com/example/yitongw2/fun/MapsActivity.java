2951f4c08f039c57ca0d4f711822773888d4f469
Yitong Wu <yitongw2@uci.edu>
2017-11-01 22:29:33 -0700

    first commit : MapsActivity, SplashOpening with Google Map/Place API
    The idea is that user search for nearby/specific cinema/restaurant and we provide options to match them together.
    
    Signed-off-by: Yitong Wu <yitongw2@uci.edu>

---
+++ app/src/main/java/com/example/yitongw2/fun/MapsActivity.java
@@ -0,0 +1,140 @@
+package com.example.yitongw2.fun;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.location.places.Place;
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, PlaceSelectionListener,
+        GoogleMap.InfoWindowAdapter, GoogleMap.OnMarkerClickListener, GoogleMap.OnInfoWindowClickListener
+{
+
+    private GoogleMap mMap;
+    private Marker marker = null;
+    private Place place = null;
+    public static final String TAG = "MapsActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        // Obtain the PlaceAutocompleteFragment and set up onSelectedListener and onError.
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        // Register a listener to receive callbacks when a place has been selected or an error has
+        // occurred.
+        autocompleteFragment.setOnPlaceSelectedListener(this);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng uci = new LatLng(33.6404952, -117.84429619999);
+        // set to custom info window (specified in info_window.xml)
+        mMap.setInfoWindowAdapter(this);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(uci, 14.0f));
+    }
+
+
+    /******************************* start PlaceSelectionListener methods ************************/
+    /**
+     * Callback invoked when a place has been selected from the PlaceAutocompleteFragment.
+     */
+    @Override
+    public void onPlaceSelected(Place place) {
+        // Get info about the selected place.
+        Log.i(TAG, "Place: " + place.getName() + place.getLatLng());
+        LatLng place_selected = place.getLatLng();
+
+        if (marker != null)
+            marker.remove();
+
+        this.place = place;
+        marker = mMap.addMarker(new MarkerOptions().position(place_selected).title(place.getName().toString()));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place_selected, 14.0f));
+    }
+
+    @Override
+    public void onError(Status status) {
+        // Handle the error.
+        Log.i(TAG, "An error occurred: " + status);
+    }
+
+    /******************************* end PlaceSelectionListener methods ************************/
+
+
+    /************************ start GoogleMap.InfoWindowAdapter methods ************************/
+    @Override
+    public View getInfoWindow(Marker m) {
+        View v = getLayoutInflater().inflate(R.layout.info_window, null);
+
+        TextView title = (TextView) v.findViewById(R.id.title);
+        title.setText(place.getName());
+
+        TextView address = (TextView) v.findViewById(R.id.address);
+        address.setText(place.getAddress());
+
+        return v;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+
+    /************************ end GoogleMap.InfoWindowAdapter methods ************************/
+
+
+    /************************ start GoogleMap.OnMarkerClickListener methods ***********************/
+    @Override
+    public boolean onMarkerClick (final Marker marker)
+    {
+        if (place != null)
+            marker.showInfoWindow();
+        return true;
+    }
+    /************************ end GoogleMap.OnMarkerClickListener methods ***********************/
+
+
+    /************************ start GoogleMap.OnInfoWindowClickListener methods ***********************/
+    @Override
+    public void onInfoWindowClick(Marker marker)
+    {
+        Toast.makeText(this, marker.getTitle(), Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/example/yitongw2/fun/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.yitongw2.fun;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
